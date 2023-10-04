e71431e691b57f6e107f3bcdf077b43a10c63517
marinavoukou <marina.eleni.voukouresli@student.forsbergsskola.se>
2023-10-04 15:36:59 +0200

    BoxColliderAndTrapAdd

---
+++ Assets/Scripts/Trap.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Trap : MonoBehaviour
+{
+       IEnumerator OnCollisionEnter(Collision other)
+       {
+              Destroy(other.gameObject);
+              yield return new WaitForSeconds(2);//wait for two seconds
+              SceneManager.LoadScene(0);//load the scene again
+       }//press return to get the usings up there
+}
