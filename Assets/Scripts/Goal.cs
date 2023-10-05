072181ad818976b30da2e73347bc81dea072b1e5
marinavoukou <marina.eleni.voukouresli@student.forsbergsskola.se>
2023-10-05 14:57:40 +0200

    LoadSceneWinSceneLoadGameDone

---
+++ Assets/Scripts/Goal.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Goal : MonoBehaviour
+{
+       IEnumerator OnCollisionEnter(Collision other)
+       {      
+              yield return new WaitForSeconds(1);//wait for two seconds
+              SceneManager.LoadScene("WinScene");//load the scene again
+       }//press return to get the usings up there
+}
--- Assets/Scripts/StartGameButton.cs
+++ Assets/Scripts/StartGameButton.cs
@@ -1,8 +1,15 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class StartGameButton : MonoBehaviour
 {
-        public void HoverStart()
+    private void FixedUpdate()
+    {
+        transform.Rotate(1, 1, 1);
+    }
+
+    public void HoverStart()
         {
             transform.localScale = Vector3.one * 1.1f;
         }
@@ -14,16 +21,20 @@ public class StartGameButton : MonoBehaviour
 
         public void ClickStart()
         {
-                
+            MeshRenderer mr = GetComponent<MeshRenderer>();
+            mr.material.color = Color.grey;
+            mr.material.SetColor("EmissionColor", Color.grey);
         }
 
         public void ClickEnd()
         {
-                
+            MeshRenderer mr = GetComponent<MeshRenderer>();
+            mr.material.color = Color.white;
+            mr.material.SetColor("EmissionColor", Color.white);
         }
 
         public void Click()
         {
-                
+            SceneManager.LoadScene("GameScene");
         }
 }
