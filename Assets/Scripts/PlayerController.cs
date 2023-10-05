def74059c6f28af65f76ae72e45c6ece293f4b2d
marinavoukou <marina.eleni.voukouresli@student.forsbergsskola.se>
2023-10-05 12:06:36 +0200

    AddJumpGravityANdBoxes

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,30 +1,35 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float jump = 300;
+    public float jumpForce = 300;
     public float speed = 0.05f; //public so its not visible an don the 
     //top so it applies otherwise it doesnt work//Field
-    
+
     // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
-      void Update()
+    void Update()
     {
-        transform.Translate(0, 0, speed);//it moves the object thats attached to
-        
-        bool isKeyWasJustPressed = Input.GetKeyDown(KeyCode.Space);
-        if (isKeyWasJustPressed)//if true then jumps
+        if (Input.GetButtonDown("Jump"))
         {
-            Rigidbody rb = GetComponent<Rigidbody>(); 
-            rb.AddForce(0, jump, 0); 
+            Rigidbody rigidBody = gameObject.GetComponent<Rigidbody>(); //finds the component 
+            rigidBody.AddForce(0, jumpForce, 0);
+
         }
-        
+
+        transform.Translate(0, 0, speed); //it moves the object thats attached to}
+    }
+
+    private void FixedUpdate()
+    {
+        Rigidbody rigidbody = gameObject.GetComponent<Rigidbody>();
+        if (rigidbody.velocity.y < -1f)
+        {
+            rigidbody.AddForce(0, -1, 0);
+        }
     }
 }
