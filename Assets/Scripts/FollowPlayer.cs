f8816a0d8d17b8b060a3305771e230cc33df1f40
marinavoukou <marina.eleni.voukouresli@student.forsbergsskola.se>
2023-10-09 11:30:40 +0200

    NoErrorsIntroNull

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -6,6 +6,7 @@ public class FollowPlayer : MonoBehaviour
         private void LateUpdate()
         {
                 GameObject player = GameObject.Find("Player");
+                if (player == null) return;
                 Vector3 target = player.transform.position + new Vector3(0, 1.48f, -4.43f);
                 transform.position = target;
         }
